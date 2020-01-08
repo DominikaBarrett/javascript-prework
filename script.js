f4cfca7459657140bb523a136bc6b0cd8a7d80a3
Dominika Barrett <barrettdomi@gmail.com>
2020-01-08 23:13:03 +0000

    papier

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+
+
 
 let computerMove = `kamień`;
 let randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -14,20 +8,46 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let playerMove = 'papier';
-let computerMove = 'nieznany ruch';
 
-printMassage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!'); 
+let computerMove = `kamień`;
+let playerMove = 'nieznany ruch';
+
+printMassage('Zagrałem ' + computerMove  + '! Jeśli Twój ruch to nożyczki, to wygrywasz!'); 
+
 if(randomNumber == 1){
-  computerMove = 'kamień';
+  computerMove = 'papier';
 }
+printMassage ( 'Zagrałem ' + computerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!');
 
 else if(randomNumber == 2){
-	computerMove = 'papier';
-}
-
-else if(randomNumber == 3){
 	computerMove = 'nożyce';
 }
 
-printMessage('Mój ruch to: ' + computerMove)
+printMassage( 'Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+else if(randomNumber == 3){
+	computerMove = 'kamień';
+
+}
+printMassage ('Zagrałem ' + computerMove + '! Jeśli Twój ruch to kamień, to jest REMIS!')
+}
+
+// let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+// console.log('Gracz wpisał: ' + playerInput);
+
+// let playerMove = 'nieznany ruch';
+// let computerMove =
+
+// if(playerInput == '1'){
+//   playerMove = 'kamień';
+// }
+
+// else if (playerInput == '2'){
+// 	playerMove ='papier';
+// }
+// else if 9(playerMove ==3){
+// 	playerMove ='nożyce';
+// }
+// printMessage('Twój ruch to: ' + playerMove);
+
